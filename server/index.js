3eb39774e3832f9d43fa65139a7f17eb501e74c8
Caio Mauro <101437705+caiomauro@users.noreply.github.com>
2023-11-04 21:29:54 -0400

    Updated

--- server/index.js
+++ server/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const axios = require("axios");
+const app = express();
+
+app.get("/api/randomuser", (req, res) => {
+  axios
+    .get("https://api.api-ninjas.com/v1/randomuser", {
+      headers: {
+        "X-Api-Key": "rYZpj+cX3gxRKnKg5eEB/w==JZ6ELBj7e8BcbTLw",
+      },
+    })
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("An error occurred");
+    });
+});
 
 const PORT = process.env.PORT || 3001;
-
-const app = express();
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
